73469e00f37893d13294f109b854968aebefc218
Stephanie Sison <sjsison27@icloud.com>
2023-01-10 11:52:24 -0800

    make CRUD for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,34 @@
 class PostsController < ApplicationController
+    # gets all posts and returns it as json
+    get '/posts' do
+        posts = Post.allocate
+        posts.to_json
+    end
+
+    # creates a new post with params and returns it as a json
+    post '/posts' do
+        posts = Post.create(
+            date: params[:date],
+            title: params[:title],
+            body: params[:body],
+            user_id: params[:user_id]
+        )
+        posts.to_json
+    end
     
+    # updates the body of the selected post
+    patch'/posts/:id' do
+        posts = Post.find(params[:id])
+        posts.update(
+            body: params[:body]
+        )
+        posts.to_json
+    end
+
+    # deletes the post from the database
+    delete '/posts/:id' do
+        posts = Post.find(params[:id])
+        posts.destroy
+        posts.to_json
+    end
 end
