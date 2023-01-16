e757a48a93c218cd85ed32421304f301fc0f30fc
Stephanie Sison <sjsison27@icloud.com>
2023-01-15 18:36:45 -0800

    update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-    # gets all users and returns it as json
+   
     get '/users' do
-        users = User.all
+        users = User.all.order(:username)
         users.to_json
     end
 
-    # creates a new user with params and return it as json
     post '/users' do
         users = User.create(
             username: params[:username],
@@ -14,7 +13,6 @@ class UsersController < ApplicationController
         users.to_json
     end
 
-    # updates the password of the user
     patch '/users/:id' do
         users = User.find(params[:id])
         users.update(
@@ -23,7 +21,6 @@ class UsersController < ApplicationController
         users.to_json
     end
 
-    # deletes the user
     delete '/users/:id' do
         user = User.find(params[:id])
         user.destroy
